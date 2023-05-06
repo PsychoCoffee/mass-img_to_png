60b18067387fe383647e11ed551cc43b90b87491
PsychoCoffeee <89361918+PsychoCoffee@users.noreply.github.com>
2023-05-07 00:42:41 +0200

    Added support for subdirectory mass conversion and added more comments
    With this, you should be able to convert images that are in several sub-directories of the output directory.

--- mass-img_to_png.py
+++ mass-img_to_png.py
@@ -13,29 +13,36 @@ input_folder = input("Input folder: ").strip('"')
 input_folder = os.path.normpath(input_folder)
 print("\n==============================================================================\n\n")
 
+#Create starting output folder
 randomnumber = random.randint(0, 2147483646)
 output_folder = "png_" + str(randomnumber)
 if '"' in output_folder:
     output_folder = output_folder.replace('"', '')
 os.makedirs(output_folder, exist_ok=True)
 
-# Count number of image files
-num_files = sum(1 for f in os.listdir(input_folder) if f.lower().endswith((".tiff", ".tif", ".jpg", ".jpeg", ".bmp", ".png")))
+num_files = 0
 
-# Counter for image files
+#Loop through the directory for the progress number
+for root, dirs, files in os.walk(input_folder):
+    for f in files:
+        if f.lower().endswith((".tiff", ".tif", ".jpg", ".jpeg", ".bmp", ".png")):
+            num_files += 1
+
 count = 0
 
-for filename in os.listdir(input_folder):
-    a = filename.lower()
-    if a.endswith(".tiff") or a.endswith(".tif") or a.endswith(".jpg") or a.endswith(".jpeg") or a.endswith(".bmp") or a.endswith(".png"):
-        count += 1
-        percentage = round((count / num_files) * 100)
-        progress = f"{count}/{num_files} - {percentage}% | {filename}"
-        print(f"Conversion {progress}")
-        image = Image.open(os.path.join(input_folder, filename))
-        image.save(os.path.join(output_folder, os.path.splitext(filename)[0] + ".png"))
+#Loop through all directories and subdirectories to convert them to .png
+for root, dirs, files in os.walk(input_folder):
+    for filename in files:
+        if filename.lower().endswith((".tiff", ".tif", ".jpg", ".jpeg", ".bmp", ".png")):
+            count += 1
+            percentage = round((count / num_files) * 100)
+            progress = f"{count}/{num_files} - {percentage}% | {filename}"
+            print(f"Conversion {progress}")
+            input_subfolder = os.path.relpath(root, input_folder)
+            output_subfolder = os.path.join(output_folder, input_subfolder)
+            os.makedirs(output_subfolder, exist_ok=True)
+            image = Image.open(os.path.join(root, filename))
+            image.save(os.path.join(output_subfolder, os.path.splitext(filename)[0] + ".png"))
 
-# Print message when done
 print("\n==============================================================================\nSuccessfully saved",count,"image files to",output_folder,"\n==============================================================================")
 input("Press enter to exit...")
-
